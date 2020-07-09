b4e442a53307acb12b31ebeff59cc6e8482e78ad
Adam James Robinson <derangedhk417@users.noreply.github.com>
2020-07-09 09:07:59 -0400

    Improved focus and zoom control, added documentation.
    Documentation includes several PDF files and the sample program for controlling the focus and zoom.

--- adam/FocusControl.py
+++ adam/FocusControl.py
@@ -11,11 +11,36 @@ class FocusController:
 		# Motor 1 is zoom.
 		# Motor 2 is focus.
 
-		self.zoom_range  = (0, 20375)
-		self.focus_range = (0, 2989) 
+		z, f = self._read_limits()
+
+		self.zoom_range  = (0, z)
+		self.focus_range = (0, f)
+
+		print("Limits:")
+		print("\tZoom:  %d"%self.zoom_range[1])
+		print("\tFocus: %d"%self.focus_range[1]) 
 
 		if not self.connection.is_open:
 			raise Exception("Failed to open serial connection.")
+
+	def _read_limits(self):
+		command_str = b"read setup_limit_1\n"
+		self.connection.write(command_str)
+
+		response   = self.readResponse()
+		value      = self._parse_response(response, command_str)
+		zoom_limit = int(value[1])
+
+		command_str = b"read setup_limit_2\n"
+		self.connection.write(command_str)
+
+		response   = self.readResponse()
+		value      = self._parse_response(response, command_str)
+		focus_limit = int(value[1])
+
+		return zoom_limit, focus_limit
+
+
 
 	def readResponse(self):
 		response_string = ""
@@ -33,10 +58,17 @@ class FocusController:
 
 	
 
-	def setZoom(self, value):
+	def setZoom(self, value, corrected=False):
 		if value < 0.0 or value > 1.0:
 			raise Exception("Values must be in the range [0.0, 1.0]")
 
+		if corrected:
+			self._set_zoom(0.0)
+			return self._set_zoom(value)
+		else:
+			return self._set_zoom(value)		
+
+	def _set_zoom(self, value):
 		value = value * (self.zoom_range[1] - self.zoom_range[0]) + self.zoom_range[0]
 		value = int(round(value))
 
@@ -50,10 +82,17 @@ class FocusController:
 
 		return value
 
-	def setFocus(self, value):
+	def setFocus(self, value, corrected=False):
 		if value < 0.0 or value > 1.0:
 			raise Exception("Values must be in the range [0.0, 1.0]")
 
+		if corrected:
+			self._set_focus(0.0)
+			return self._set_focus(value)
+		else:
+			self._set_focus(value)
+
+	def _set_focus(self, value):
 		value = value * (self.focus_range[1] - self.focus_range[0]) + self.focus_range[0]
 		value = int(round(value))
 
